9e257f4135a83ab6729f747e90277eeab31ab1e7
tania rosas-jimenez <rosastania@yahoo.com>
2014-10-30 11:05:00 -0700

    Added a header.php and footer.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<?php 
+
+<?php 
 	//view is probably where you'll get info from the model or have html code
 	//__DIR__ concatinates
 	//
-		require_once(__DIR__ . "/controller/create-db.php");
- 	?>
-</body>
-</html>
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/controller/create-db.php");
+	require_once(__DIR__ . "/view/footer.php");
+?>
