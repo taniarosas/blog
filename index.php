d45620b53571b66792222bb483e1eb583dc76c9f
tania rosas-jimenez <rosastania@yahoo.com>
2014-10-30 11:14:56 -0700

    Added navigation.php and post.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	//__DIR__ concatinates
 	//
 	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/navigation.php");
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
 ?>
