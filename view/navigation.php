68d21696666be21c762736a8f142cccc6d6a2095
tania rosas-jimenez <rosastania@yahoo.com>
2014-11-02 17:22:33 -0800

    created a variable in config.php and put require_once() in navigation.php

--- view/navigation.php
+++ view/navigation.php
@@ -1,5 +1,8 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
 <nav>
 	<ul>
-		<li><a href="../post.php"> Blog Post Form</a></li>
+		<li><a href="<?php echo $path . "post.php"?>"> Blog Post Form</a></li>
 	</ul>
 </nav>
