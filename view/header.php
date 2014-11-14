1ab77aff830b41aef5847842ecd5ee72d2417e45
tania rosas-jimenez <rosastania@yahoo.com>
2014-11-14 11:19:00 -0800

    Added bootstrap and linked/script them to our code

--- view/header.php
+++ view/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
 		<!--it links the css stylesheet to our php file-->
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
