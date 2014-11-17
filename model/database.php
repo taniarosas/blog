af87347eeafaa0783286e026ca33d0f480bacd8d
tania rosas-jimenez <rosastania@yahoo.com>
2014-11-17 12:04:48 -0800

    Added comments to database.php for videos 27 & 28

--- model/database.php
+++ model/database.php
@@ -14,13 +14,17 @@
 		//private makes the variables unavailable to other classes
 		private $database;
 
+		//local variables
+		//it is the consructor for the class
+		//this construct function is needed to make objects
 		public function __construct($host, $username, $password, $database){
+			//setting the global variables to our local variables
 			$this->host = $host;
 			$this->username = $username;
 			$this->password = $password;
 			$this->database = $database;
 		}
-
+		//this function opens the connection
 		public function openConnection() {
 			this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
@@ -29,11 +33,14 @@
 				die("<p>Error: " . $this->connection->connect_error . "</p>");
 			}
 		}
+		//this function closes the connection
 		public function closeConnection() {
 			if(isset($this->connection)) {
 				this->connection->close();
 			}
 		}
+		//every time we call the query we have to write a string
+		//in the end it will be passed as a variable 
 		public function query($string) {
 			this->openConnection();
 
