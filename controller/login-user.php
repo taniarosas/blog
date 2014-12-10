170c4813b87271152f21db6f57f8811265f574f3
tania rosas-jimenez <rosastania@yahoo.com>
2014-12-09 18:16:05 -0800

    coded for video 48; checking the passwords for login

--- controller/login-user.php
+++ controller/login-user.php
@@ -6,3 +6,17 @@
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+	if($query->num_rows == 1) {
+		$row = $query->fetch_array();
+
+		if($row["password"] === crypt($password, $row["salt"])){
+			echo "<p>Login Successful</p>";
+		}
+		else{
+			echo "<p>Invalid username and password</p>";
+		}
+	}
+	else{
+		echo "<p>Invalid username and password</p>";
+	}
