952ec86182a62de043b0e11a4aa5b6c47d874d21
tania rosas-jimenez <rosastania@yahoo.com>
2014-12-16 09:16:27 -0800

    commented in create-user.php

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,4 +1,6 @@
 <?php
+	//requiring the config file
+	//this is going to be for creating a user
 	require_once(__DIR__ . "/../model/config.php");
 		/*require_once(__DIR__ . "/../controller/login-verify.php");
 	if(!authenticateUser()){
@@ -6,15 +8,29 @@
 		die();
 	}*/
 
-
+	//we are creating a variable named email
+	//were filtering an input and getting it from the post method
+	//getting it by the name email
+	//santizing the input email
 	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	//we are creating a variable named username
+	//were filtering an input and getting it from the post method
+	//sanitizing the input username by string 
+	//getting it by the name username
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	//we are creating a variable named password 
+	//were filtering an input and getting it from the post method
+	//getting it by the name password
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
-
+	//using $5$ 5000 times
+	//uniqid generates a random number
 	$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
-
+	//using password n salt to create a crypt password
 	$hashedPassword = crypt($password, $salt);
-
+	//creating a query variable
+	//using the session because it has a connection and this connection is a connection to our databse
+	//we are querying
+	//were inserting in the users table and setting info "space needs to be after set"
 	$query = $_SESSION["connection"]->query("INSERT INTO users SET "
 		. "email = '$email',"
 		. "username = '$username',"
