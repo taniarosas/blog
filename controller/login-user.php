e6fdf0ef677f194ad6127ddde072cbeb92ce575e
tania rosas-jimenez <rosastania@yahoo.com>
2014-12-15 20:19:57 -0800

    commented in login-user.php and login-verify.php

--- controller/login-user.php
+++ controller/login-user.php
@@ -4,7 +4,7 @@
 
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
-
+	//selects our salt and password from our users table when our username is = to the username that the user input
 	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
 
 	if($query->num_rows == 1) {
--- controller/login-verify.php
+++ controller/login-verify.php
@@ -1,7 +1,8 @@
 <?php
 	//require the config file to get to the $path variable
 	require_once(__DIR__ . "/../model/config.php");
-
+	//when it is required by a by webpage they will have access to the function
+	//and the file will be able to use this function to determine whether or not the user is logged in
 	function authenticateUser(){
 		if(!isset($_SESSION["authenticated"])){
 			return false;
